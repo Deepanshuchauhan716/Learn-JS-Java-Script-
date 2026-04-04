b24a59f7280b336554f2f684a3c4e85a977027a2
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-04 14:19:27 +0530

    Input data console in page without NULL

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ let h1 = document.querySelector("h1");
 h1.addEventListener("click", function() {
     h1.style.color = "red";
 })
+
+let input = document.querySelector("input");
+
+input.addEventListener("input", function(evt) {
+    if (evt.data !== null) {
+
+        console.log(evt.data);
+    }
+})
