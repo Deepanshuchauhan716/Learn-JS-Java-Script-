05a1c3d8279ea81f26c264e538465f6214ba1f82
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-04 13:07:10 +0530

    create Element through js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let h1 = document.querySelector("h1");
-h1.textContent = "Hello deepanshu";
-
-let a = document.querySelector("a");
-a.setAttribute("href", "https://www.google.com");
+let h1 = document.createElement("h1");
+h1.textContent = "Hello js";
+document.querySelector("body").append(h1);
