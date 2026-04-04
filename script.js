edfdae3161c4a00d4007262c2afc209bb0a771f5
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-04-04 16:04:17 +0530

    upload file text change into choose file

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let inp = document.querySelector("input");
 butn.addEventListener("click", function() {
     inp.click();
 })
+
+inp.addEventListener("change", function(dets) {
+    console.dir(dets.target.files[0].name);
+    butn.textContent = dets.target.files[0].name;
+})
